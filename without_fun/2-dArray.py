5a9585120a09741e9577606a8b37c337254dd14f
Preksha1998 <prekshasheth1998@gmail.com>
2020-08-04 10:07:11 -0600

    second commit

---
+++ without_fun/2-dArray.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr = np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print("*** 2-D Array ***\n")
+print(arr)
+
+arr2 = np.array([[11,12,13,14,15],[16,17,18,19,20]])
+print("\n\n*** 2-D Array ***\n")
+print(arr2)
